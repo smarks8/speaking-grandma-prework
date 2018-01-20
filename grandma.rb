b9dcbf58fd32b9d7d5d26821819b1cbc8c99e41f
Stacey  Marks <staceyamarks@gmail.com>
2018-01-20 19:19:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOOPUMPKIN!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
