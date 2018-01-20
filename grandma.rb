54692c6ee0f2c765132b229d5a6bfcbb32b223ae
Stacey  Marks <staceyamarks@gmail.com>
2018-01-20 19:24:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
-  end
   
   else 
     return "HUH?! SPEAK UP, SONNY!" 
